7aba77d6089a0a788675e81d608857ee27cc0b7b
Cameron Sullivan <arrowc11@gmail.com>
2017-09-13 16:53:21 -0400

    Final HW1

--- HW1/palindromeTester.cpp
+++ HW1/palindromeTester.cpp
@@ -1,51 +1,46 @@
 /*
-* Cameron Sullivan - COS 221 - HW1
-* Palindrome Tester: This program takes string input and evaluates the strings.
-* It will determine if the string is a palindrome, and print its findings.
+* Cameron Sullivan - COS 221 - HW1 - 9-13-17
+* Palindrome Tester: This program takes string input and evaluates the string.
+* It determines if the string is a palindrome, and prints whether the string is a palindrome or not.
 */
 
 #include <string>
 #include <iostream>
-#include <algorithm>     // For string transformation.
 
 using namespace std;
 
+// Function prototype
 bool isPalindrome(const string&);
 
 int main() {
-	string test1 = "Able was I ere I saw Elba";
-	string test2 = "Boo hoo hoo";
-	string test3 = "dad";
-	string test4 = "rage";
-	string test5 = "EPEPe";
+	
+	// Prompt for input and accept it into testString.
+	string testString;
+	cout << "Enter a string to be palindrome tested: ";
+	getline(cin, testString);
 
-	isPalindrome(test1);
-	isPalindrome(test2);
-
-	isPalindrome(test3);
-	isPalindrome(test4);
-	isPalindrome(test5);
+	// Run the palindrome tester with the test string.
+	isPalindrome(testString);
 
 	return 0;
 }
 
+// Palindrome-testing function.
 bool isPalindrome(const string& s) {
-	string tempString;
-	tempString.assign(s);
-	// CITE
-	std::transform(tempString.begin(), tempString.end(), tempString.begin(), ::tolower);
  	
-	int high = tempString.length() - 1;
+	// Get the index of the last character of the string.
+	int high = s.length() - 1;
 
+	// Loop until the low and high indices reach the middle character (odd) or go past each other (even).
 	for (int low = 0; low < high; low++, high--) {
-		
-		if (tempString.at(low) != tempString.at(high)) {
+		// If the two characters are not the same, the string is not a palindrome.
+		if (tolower(s[low]) != tolower(s[high])) {
 			cout << "'" << s << "' is not a palindrome." << endl;
 			return 0;
 		}
 	}
-
+	
+	// If all beginning and ending characters are the same, the string is a palindrome.
 	cout << "'" << s << "' is a palindrome." << endl;
-
 	return 1;
 }
