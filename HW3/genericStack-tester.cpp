313da74a9ddc66e46dcf72ba4fc48e981e042b74
Cameron Sullivan <arrowc11@gmail.com>
2017-09-26 14:18:41 -0400

    Ref: exception use

--- HW3/genericStack-tester.cpp
+++ HW3/genericStack-tester.cpp
@@ -1,16 +1,19 @@
 /*
-*
+* COS 221 - Cameron Sullivan - HW3
+* genericStack Tester: This program creates a stack object and shows the use of each stack function.
 */
 
 #include <iostream>
-#include <string>
 #include <vector>
 #include "genericStack.h"
 
 using namespace std;
 
 int main() {
+	// Create a Stack object and specify type string.
 	Stack<string> stringStack;
+
+	// Testing and output for the user- test all functions.
 	cout << "Pushing 'Bird,' 'Cat,' and 'Dog' to the vector..." << endl;
 	stringStack.push("Bird");
 	stringStack.push("Cat");
@@ -21,11 +24,13 @@ int main() {
 	cout << "Popping two elements..." << endl;
 	stringStack.pop();
 	stringStack.pop();
+	cout << "Stack size: " << stringStack.getSize() << endl;
 	cout << "Stack is empty? " << stringStack.empty() << endl;
 	cout << "Stack peek: " << stringStack.peek() << endl;
 	cout << "Popping an element..." << endl;
 	stringStack.pop();
 	cout << "Stack is empty? " << stringStack.empty() << endl;
+	cout << "peek: " << stringStack.peek() << endl;
 
 	return 0;
 }
--- HW3/genericStack.h
+++ HW3/genericStack.h
@@ -1,5 +1,8 @@
 /* COS 221 - Cameron Sullivan - HW3
-*
+* Generic stack header: This header includes function implementation. 
+* This is because generics are involved, so implementation must done in the header.
+* This program simulates a typical computer stack: it allows the user to push data on and pop data off the stack, and also allows them to peek at the top element and check the size and emptiness of the stack.
+* This program uses the  generic type to allow the user to create a stack of any variable type, and it uses a vector to allow for stack dynamic sizing.
 */
 
 #include <vector>
@@ -9,6 +12,7 @@ template<typename T>
 class Stack 
 {
 public:
+	// Public prototypes.
 	Stack();
 	bool empty() const;
 	T peek() const;
@@ -17,37 +21,53 @@ public:
 	int getSize() const;
 
 private:
+	// Private variables.
 	vector<T> elements;
 	int size;
 };
 
+// Empty constructor
 template<typename T>
 Stack<T>::Stack() {
 }
 
+// empty() - Returns 1 if the stack is empty, 0 if the stack contains elements.
 template<typename T>
 bool Stack<T>::empty() const {
-	return (elements.size() == 0);
+		return (elements.size() == 0);
 }
 
-
+// peek() - Returns the element on the top of the stack.
 template<typename T>
 T Stack<T>::peek() const {
-	return elements.at(elements.size() - 1);
+	try {
+		
+		if (getSize() != 0) {	
+			return elements.at(elements.size() - 1);
+		}
+		else {
+			throw runtime_error("Cannot peek when the stack is empty!");
+		}
+	}
+	catch (runtime_error& e) {
+		cout << e.what() << endl;
+		return NULL;
+	}
 }
 
+// push() - Adds a new element on the top of the stack.
 template<typename T>
 void Stack<T>::push(T value) {
 	elements.push_back(value);
 }
 
-
+// pop() - Pops the top element off the stack.
 template<typename T>
 void Stack<T>::pop() {
 	elements.pop_back();
 }
 
-
+// getSize() - returns the number of elements in the stack.
 template<typename T>
 int Stack<T>::getSize() const {
 	return elements.size();
