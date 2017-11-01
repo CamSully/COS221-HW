99ec1b24cb74bc46d8f0489071e68cd28484f2b2
Cameron Sullivan <arrowc11@gmail.com>
2017-10-31 21:34:29 -0400

    Functional HW7

--- HW7/removeTester.cpp
+++ HW7/removeTester.cpp
@@ -14,19 +14,30 @@ int main() {
 	const string sentence = "DURING the whole of a dull, dark, and soundless day in the autumn of the year, when the clouds hung oppressively low in the heavens, I had been passing alone, on horseback, through a singularly dreary tract of country; and at length found myself, as the shades of the evening drew on, within view of the melancholy House of Usher.";
 
 	parseString(sentence);
+
+	list.remove("of");
 	
-//	for (int i = 0; i < list.getSize(); i++) {
-//		cout << list.get(i) << endl;
-//	}
+	for (int i = 0; i < list.getSize(); i++) {
+		cout << list.get(i) << " ";
+	}
+	cout << endl;
 
 	return 0;
 }
 
 void parseString(string sentence) {
-	int startIndex;
-	int stopIndex;
+	int startIndex = 0;
+	int numChars = 0;
 	bool firstLetter = true;
-	string word;
+	string word = "";
+
+	for (int i = 0; i < sentence.size(); i++) {
+		if (!isalpha(sentence.at(i)) && !isspace(sentence.at(i))) {
+			sentence.erase(i, 1);
+		}
+	}
+
+	int length = sentence.size();
 
 	for (int i = 0; i < sentence.size(); i++) {
 		if (isalpha(sentence.at(i))) {
@@ -34,13 +45,19 @@ void parseString(string sentence) {
 				startIndex = i;
 				firstLetter = false;
 			}
+			numChars++;
 		}
+		// Char is a space.
 		else {
-			stopIndex = i - 1;
-			word = sentence.substr(startIndex, stopIndex);
+			// +1 because last index is exclusive.
+			word = sentence.substr(startIndex, numChars);
 			list.add(word);
-			cout << word << endl;
+		//	cout << word << endl;
 			firstLetter = true;
+			numChars = 0;
 		}
 	}
+	word = sentence.substr(startIndex, numChars);
+	list.add(word);
+	//cout << word << endl;
 }
